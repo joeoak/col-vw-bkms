3f8f04ef2a87022cb9756fb76039367e122af2a6
Joe Oak <josephjoak@gmail.com>
2021-04-16 01:17:22 -0700

    Built open folder functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,53 @@ import React, { useEffect, useState } from 'react';
 export const App = () => {
   const [bookmarks, setBookmarks] = useState([]);
   const [openFolders, setOpenFolders] = useState([]);
-  const [openFoldersIds, setOpenFoldersIds] = useState([]);
+  const [openFoldersIds, setOpenFoldersIds] = useState([{ id: '0', parentId: null }]);
 
   useEffect(() => {
     chrome.bookmarks.getTree((tree) => {
-      setBookmarks(tree[0].children);
+      setBookmarks(tree);
     });
   }, []);
 
   useEffect(() => {
     if (bookmarks.length > 0) {
-      setOpenFolders([<Column callback={updateOpenFoldersIds} bookmarks={bookmarks} />]);
-      findAndSetOpenFolders(bookmarks);
+      updateOpenFolders();
     }
   }, [bookmarks]);
 
   useEffect(() => {
-    console.log(openFoldersIds);
-    setOpenFolders([<Column callback={updateOpenFoldersIds} bookmarks={bookmarks} />]);
-    findAndSetOpenFolders(bookmarks);
+    updateOpenFolders();
   }, [openFoldersIds]);
-
-  const updateOpenFoldersIds = (id) => {
-    if (!openFoldersIds.includes(id)) {
-      setOpenFoldersIds((openFoldersIds) => [...openFoldersIds, id]);
-    }
-  };
 
   const findAndSetOpenFolders = (arr) => {
     arr.forEach((bookmark) => {
       if (bookmark.children) {
-        if (openFoldersIds.includes(bookmark.id)) {
+        if (openFoldersIds.some((obj) => obj.id === bookmark.id)) {
           setOpenFolders((openFolders) => [...openFolders, <Column callback={updateOpenFoldersIds} bookmarks={bookmark.children} />]);
         }
         return findAndSetOpenFolders(bookmark.children);
       }
     });
+  };
+
+  const updateOpenFolders = () => {
+    setOpenFolders([]);
+    findAndSetOpenFolders(bookmarks);
+  };
+
+  const updateOpenFoldersIds = (folderObj) => {
+    if (!openFoldersIds.some((obj) => obj.id === folderObj)) {
+      let newArr = [folderObj];
+      let targetFolder = folderObj;
+
+      while (targetFolder.id !== '0') {
+        let parentFolder = openFoldersIds.find((obj) => obj.id === targetFolder.parentId);
+        newArr.unshift(parentFolder);
+        targetFolder = parentFolder;
+      }
+
+      setOpenFoldersIds(newArr);
+    }
   };
 
   return <div className="columns">{openFolders}</div>;
@@ -49,7 +60,7 @@ const Column = (props) => {
   return (
     <div className="column">
       {props.bookmarks.map((bookmark) => (
-        <Bookmark callback={props.callback} id={bookmark.id} url={bookmark.url}>
+        <Bookmark callback={props.callback} bookmark={bookmark}>
           {bookmark.title}
         </Bookmark>
       ))}
@@ -59,12 +70,12 @@ const Column = (props) => {
 
 const Bookmark = (props) => {
   const onClick = () => {
-    return props.callback(props.id);
+    return props.callback({ id: props.bookmark.id, parentId: props.bookmark.parentId });
   };
 
   return (
     <div className="bookmark">
-      <a href={props.url} onClick={() => onClick()}>
+      <a href={props.bookmark.url} onClick={() => onClick()}>
         {props.children}
       </a>
     </div>
